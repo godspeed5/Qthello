bd648ba49b96a6d68296081496cdc0d2244eb496
parsidd <ee17b059@smail.iitm.ac.in>
2020-10-01 12:09:24 +0530

    Changed pygame window name, took out common instructions from loop

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 from qiskit import *
 from pygame.locals import *
+
 """
  Example program to show using an array to back a grid on-screen.
  
@@ -17,13 +18,14 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+BLUE = (0, 0, 255)
  
 # This sets the WIDTH and HEIGHT of each grid location
-WIDTH = 100
-HEIGHT = 100
+WIDTH = 80
+HEIGHT = 80
  
 # This sets the margin between each cell
-MARGIN = 6
+MARGIN = 3
  
 # Create a 2 dimensional array. A two dimensional
 # array is simply a list of lists.
@@ -49,11 +51,33 @@ WINDOW_SIZE = [WIDTH*8+MARGIN*7, WIDTH*8+MARGIN*7]
 screen = pygame.display.set_mode(WINDOW_SIZE)
  
 # Set title of screen
-pygame.display.set_caption("Array Backed Grid")
+pygame.display.set_caption("Qthello")
  
 # Loop until the user clicks the close button.
 done = False
- 
+
+# Set the font
+font=pygame.font.SysFont('arial', 40)
+
+centerlist=[]
+for row in range(8):
+    alist=[]
+    for column in range(8):
+        alist.append(((MARGIN + WIDTH) * column + MARGIN + WIDTH/3, (MARGIN + HEIGHT) * row + MARGIN + HEIGHT/3))
+    centerlist.append(alist)
+
+s_text=font.render('S', True, BLUE)
+s_rects=[centerlist[0][0],centerlist[0][7],centerlist[7][0],centerlist[7][7]]
+
+cx_text=font.render('CX', True, BLUE)
+cx_rects=[centerlist[1][1],centerlist[1][6],centerlist[6][1],centerlist[6][6]]
+
+h_text=font.render('H', True, BLUE)
+h_rects=[centerlist[0][2],centerlist[2][0],centerlist[0][5],centerlist[5][0],centerlist[7][5],centerlist[7][2],centerlist[5][7],centerlist[2][7]]
+
+x_text=font.render('X', True, BLUE)
+x_rects=[centerlist[2][2],centerlist[2][5],centerlist[5][2],centerlist[5][5]]
+
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
  
@@ -72,12 +96,6 @@ while not done:
  
     # Set the screen background
     screen.fill(BLACK)
-    centerlist=[]
-    for row in range(8):
-    	alist=[]
-    	for column in range(8):
-    		alist.append(((MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN))
-    	centerlist.append(alist)
  
     # Draw the grid
     for row1 in range(8):
@@ -85,7 +103,7 @@ while not done:
             color = WHITE
             if grid[row1][column1] == 1:
                 color = GREEN
-            if grid[row1][column1]==2:
+            if grid[row1][column1] == 2:
             	color = BLACK
             pygame.draw.rect(screen,
                              color,
@@ -93,23 +111,18 @@ while not done:
                               (MARGIN + HEIGHT) * row1 + MARGIN,
                               WIDTH,
                               HEIGHT])
-            font=pygame.font.SysFont('arial', 40)
-            text=font.render('S', True, (0, 0, 0))
-            rects=[centerlist[0][0],centerlist[0][7],centerlist[7][0],centerlist[7][7]]
-            for rect in rects:
-            	screen.blit(text, rect)
-            text=font.render('CX', True, (0, 0, 0))
-            rects=[centerlist[1][1],centerlist[1][6],centerlist[6][1],centerlist[6][6]]
-            for rect in rects:
-            	screen.blit(text, rect)
-            text=font.render('H', True, (0, 0, 0))
-            rects=[centerlist[0][2],centerlist[2][0],centerlist[0][5],centerlist[5][0],centerlist[7][5],centerlist[7][2],centerlist[5][7],centerlist[2][7]]
-            for rect in rects:
-            	screen.blit(text, rect)
-            text=font.render('X', True, (0, 0, 0))
-            rects=[centerlist[2][2],centerlist[2][5],centerlist[2][5],centerlist[5][5]]
-            for rect in rects:
-            	screen.blit(text, rect)
+            
+            for rect in s_rects:
+            	screen.blit(s_text, rect)
+            
+            for rect in cx_rects:
+            	screen.blit(cx_text, rect)
+            
+            for rect in h_rects:
+            	screen.blit(h_text, rect)
+            
+            for rect in x_rects:
+            	screen.blit(x_text, rect)
  
     # Limit to 60 frames per second
     clock.tick(60)
