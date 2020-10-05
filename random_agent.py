848827f4fb64e04ecc475c712c98569b956ccdef
parsidd <ee17b059@smail.iitm.ac.in>
2020-10-05 22:16:48 +0530

    Added measurement in random agent.

--- random_agent.py
+++ random_agent.py
@@ -3,17 +3,28 @@ import numpy as np
 class random_agent:
     def __init__(self, name = "Random Agent"):
         self.name = name
+        self.move = []
     
-    def ret_move(self, isvalid, bag):
+    def choose_pos(self, isvalid):
+        ii = np.where(isvalid == 1)
+        l = len(ii[0])
+        x, y = np.random.randint(0,l), np.random.randint(0,l)
+        self.move = [ii[0][x],ii[1][y]]
+
+    
+    def ret_move(self, isvalid, bag, measured):
         i = 0
+        p = np.random.randint(1,100)
+        self.choose_pos(isvalid)
+        while(p > 80 or bag.count(0)==6):
+            if(measured[self.move] == 1):
+                self.choose_pos(isvalid)
+                continue
+            return self.move, 0
         qubit = 0
         while(i == 0):
             qubit = np.random.randint(1,7)
             if bag[qubit] == 0:
                 continue
             i = 1
-        ii = np.where(isvalid == 1)
-        l = len(ii[0])
-        x, y = np.random.randint(0,l), np.random.randint(0,l)
-        move = [ii[0][x],ii[1][y]]
-        return move,qubit
+        return self.move,qubit
